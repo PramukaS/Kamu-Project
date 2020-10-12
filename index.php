14146dfb7779cd066fc0d5acfe26405e7a34a187
Pramuka Seneviratne <41991286+PramukaS@users.noreply.github.com>
2020-10-12 19:30:32 +0530

    create separate footer files
    footer.php and footer.css

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Quicksand:300,500" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
-    <link href="css/index.css" rel="stylesheet" type="text/css" />
+    <link href="css/index.css" rel="stylesheet" type="text/css">
     <title>Kamu.LK</title>
 </head>
 <body>
@@ -49,32 +49,6 @@
             </div>
         </div>
     </div>
-
-    <div class="footer">
-        <div class="footer-section about">
-            <h1 id="logo-text">Kamu.lk</h1>
-            <p id="footer">“Kamu.lk” is a project with the main goal of promoting healthy food habits in order to prevent 
-                noncommunicable diseases in the Sri Lankan community.</br></br>
-                Copyright 2020 &copy; Kamu.Lk
-             </p>
-        </div>
-
-        <div class="footer-section social">
-            <a href="https://www.facebook.com/"><img id="icon" src="images/facebook.jpg" alt="facebook" ></a>
-            <a href="https://www.instagram.com/"><img id="icon" src="images/instagram.jpg" alt="instagram" ></a>
-            <a href="https://twitter.com/"><img id="icon" src="images/twitter.jpg" alt="twitter" ></a> 
-            <a href="https://www.youtube.com/"><img id="icon" src="images/youtube.jpg" alt="youtube" ></a>
-        </div>
-
-        <div class="footer-section links">
-            <ul type="none">
-                <li><a href="#">Privacy Policy</a></li>
-                <li><a href="#">Terms of Service</a></li>
-                <li><a href="#">Contact Us</a></li>
-                <li><a href="#">About Us</a></li>
-            </ul>
-        </div>
-    </div>
-
+    <?php include('footer.php'); ?>
 </body>
 </html>

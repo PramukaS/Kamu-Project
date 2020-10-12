debc4ff7ceb134b1331d9fb25410db4dc372c4a4
Pramuka Seneviratne <41991286+PramukaS@users.noreply.github.com>
2020-10-12 18:32:14 +0530

    add footer to index.php
    add some icon images to images folder

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Quicksand:300,500" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
     <link href="css/index.css" rel="stylesheet" type="text/css" />
     <title>Kamu.LK</title>
 </head>
 <body>
-    <div class="index">
+    <div class="container">
         <div class="topnav">
             <ul type="none">
                 <li><a id="login" href="">Login</a></li>
@@ -18,44 +19,62 @@
                 <a href=""><img id="logo" src="images/logo.png" alt="logo" ></a>  
         </div>
 
-        <div class="user">
-            <div><img id="diet_plan" src="images/diet_plan.png" alt="diet_plan" ></div>
-            <h1>
-                Eat Healthy.</br>Stay Healthy. 
-            </h1>
-            <p>We provide personalized meal plans with expert advice.
-                Order food and get delivered it to your door steps.</p>
-            <div class="button"> <a id="SignUp" href="">Sign Up</a></div>
-        </div>
-
-        <div class="restaurant">
-            <div><img id="restaurant" src="images/restaurant.png" alt="restaurant" ></div>
-            <h1 id="restaurant">
-                Add Your</br>Restaurant Here.
-            </h1>
-            <p> You can add your meals and get connected with your customers.</p>
-            <div class="button"> <a id="SignUp" href="">Sign Up as Restaurant</a></div>
-        </div>
-
-        <div class="driver">
-            <div><img id="driver" src="images/driver.png" alt="driver" ></div>
-            <h1>
-                Earn Money</br>From Kamu.lk
-            </h1>
-            <p>You can earn money by delivering foods.</p>
-            <div class="button"> <a id="SignUp" href="">Sign Up as Driver</a></div>
-        </div>
-
-
-        <div class="footer">
-            <div class="footer-content">
-                <div class="footer-section-about"></div>
-                <div class="footer-section-links"></div>
-                <div class="footer-section-contact-forms"></div>
+        <div class="main">
+            <div class="user">
+                <div><img id="diet_plan" src="images/diet_plan.png" alt="diet_plan" ></div>
+                <h1>
+                    Eat Healthy.</br>Stay Healthy. 
+                </h1>
+                <p>We provide personalized meal plans with expert advice.
+                    Order food and get delivered it to your door steps.</p>
+                <div class="button"> <a id="SignUp" href="">Sign Up</a></div>
             </div>
-            <div class="footer-bottom"> </div>
 
+            <div class="restaurant">
+                <div><img id="restaurant" src="images/restaurant.png" alt="restaurant" ></div>
+                <h1 id="restaurant">
+                    Add Your</br>Restaurant Here.
+                </h1>
+                <p> You can add your meals and get connected with your customers.</p>
+                <div class="button"> <a id="SignUp" href="">Sign Up as Restaurant</a></div>
+            </div>
 
+            <div class="driver">
+                <div><img id="driver" src="images/driver.png" alt="driver" ></div>
+                <h1>
+                    Earn Money</br>From Kamu.lk
+                </h1>
+                <p>You can earn money by delivering foods.</p>
+                <div class="button"> <a id="SignUp" href="">Sign Up as Driver</a></div>
+            </div>
         </div>
+    </div>
+
+    <div class="footer">
+        <div class="footer-section about">
+            <h1 id="logo-text">Kamu.lk</h1>
+            <p id="footer">“Kamu.lk” is a project with the main goal of promoting healthy food habits in order to prevent 
+                noncommunicable diseases in the Sri Lankan community.</br></br>
+                Copyright 2020 &copy; Kamu.Lk
+             </p>
+        </div>
+
+        <div class="footer-section social">
+            <a href="https://www.facebook.com/"><img id="icon" src="images/facebook.jpg" alt="facebook" ></a>
+            <a href="https://www.instagram.com/"><img id="icon" src="images/instagram.jpg" alt="instagram" ></a>
+            <a href="https://twitter.com/"><img id="icon" src="images/twitter.jpg" alt="twitter" ></a> 
+            <a href="https://www.youtube.com/"><img id="icon" src="images/youtube.jpg" alt="youtube" ></a>
+        </div>
+
+        <div class="footer-section links">
+            <ul type="none">
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms of Service</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">About Us</a></li>
+            </ul>
+        </div>
+    </div>
+
 </body>
 </html>

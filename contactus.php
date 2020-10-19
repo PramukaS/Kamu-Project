8052921610cb66b57a6d065a5d24cf0568f608b0
shifnasm <56575819+shifnasm@users.noreply.github.com>
2020-10-19 20:57:40 +0530

    kamu project-shifna
    About us , contact us and delivery driver signup pages .

---
+++ contactus.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<link rel='stylesheet' type="text/css" href="css/css1.css">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand:300,500" rel="stylesheet">
+	<title>contact us form</title>
+</head>
+<body class="body2">
+	<div class="index">
+        <div class="topnav">
+            <ul type="none">
+                <li><a id="login" href="login.php">Login</a></li>
+                <li><a href="aboutus.php">About Us</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Restaurants</a></li>
+            </ul>
+                <a href=""><img id="logo" src="images/logo.png" alt="logo" ></a>  
+        </div>
+    </div>		
+	
+
+<div class="container">
+
+	<div class="contact-title">
+		
+    		<h1>KAMU.lk</h1>
+    		<br>
+    		<h2>We serve you the best</h2>
+    	
+  	</div>
+
+<div class="text">CONTACT US</div>
+	<div class="contact-info">
+		<img src = "images/contact1.jpg" height="128px" width="160px" align="left" >
+      <table class="table">
+        <tbody>
+            <tr>
+                <td>Hotline:</td>
+                <td>+ 94 77 2342347</td>
+            </tr>
+            
+            
+            <tr>
+                <td>Email:</td>
+                <td>kamuinfo@gmail.com</td>
+            </tr>
+
+            <tr>
+                <td>Facebook:</td>
+                <td>Kamu.lk</td>
+            </tr>
+          </tbody>
+      
+      </table>
+    </div>
+			<form class="form1" id="contact-form" method="post" action="#">
+
+				<div class="form-row">
+					<div class="input-data">
+						<input type="text" placeholder="Your name" name="name" required><br>
+					</div>
+				</div>
+				<div class="form-row">
+					<div class="input-data">
+						<input type="email" placeholder="Your email" name="email" required><br>
+					</div>
+				</div>
+				<div class="form-row">
+					<div class="input-data">
+						<input type="subject" placeholder="Subject" name="subject" required><br>
+					</div>
+				</div>
+
+					<div class="form-row">
+						<div class="input-data textarea">
+							<textarea name="message" placeholder="Message" rows="5" required></textarea><br>
+						</div>
+					</div>
+
+					<div class="submit-btn">
+                    <div class="form-row">
+                        <div class="inner">
+                        <input type="submit" value="Submit" name="submit">
+                        </div>
+                    </div>
+            </div>
+			</form>
+
+
+	</div>
+	<div class="footer">
+            <div class="footer-content">
+                <div class="footer-section-about"></div>
+                <div class="footer-section-links"></div>
+                <div class="footer-section-contact-forms"></div>
+            </div>
+            <div class="footer-bottom"> </div>
+
+
+        </div>
+
+		
+
+</body>
+</html>

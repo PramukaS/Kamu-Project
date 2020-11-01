555138a9bd51f0adce58cb1313b04aee1c27b600
Pramuka Seneviratne <41991286+PramukaS@users.noreply.github.com>
2020-11-01 19:38:41 +0530

    my new

---
+++ nav.php
@@ -0,0 +1,39 @@
+<div class="navbar">
+    <div class="logo">
+        <img src="images/logo.png" alt="logo" width="125px">
+    </div>
+    <div class="nav-right">
+        <h3>Hi Nutritionist</h3>
+            <div class="dropdown">
+                <button class="dropbtn"><img  src="images/user.png" alt="user" width="50px"></button>
+                <div class="dropdown-content">
+                    <a href="#">My Profile</a>
+                    <a href="#">Log Out</a>
+                </div>
+            </div>
+    </div>
+</div>
+<div class="sidebar">
+    <ul>                
+        <li> <a href="dash.php">DASHBOARD</a></li>
+        <li> CLIENTS
+            <ul style="display: none;">
+                <li><a href="">add</a></li>
+                <li><a href="">view</a></li>
+            </ul>
+        </li>
+        <li> FOOD
+            <ul style="display: none;">
+                <li><a href="">add</a></li>
+                <li><a href="">view</a></li>
+            </ul>
+        </li>
+        <li>POST
+            <ul style="display: none;">
+                <li><a href="">add</a></li>
+                <li><a href="">view</a></li>
+            </ul>
+        </li>
+        <li><a href="">CONTACT ADMINISTRATOR</a></li>
+    </ul>
+</div>

86286730a8556817ddfc044c4fa4fe06a78f0528
TMWEE <pathmikaweerarathna@gmail.com>
2020-11-16 18:12:37 +0530

    seller/driver nw update#

--- tm/Driver/dash_include/nav.php
+++ tm/Driver/dash_include/nav.php
@@ -3,7 +3,7 @@
         <img src="img/logo.png" alt="logo" width="125px">
     </div>
     <div class="nav-right">
-        <h3>Hi DRIVER</h3>
+        <h3>Hi Driver</h3>
             <div class="dropdown">
                 <button class="dropbtn"><img  src="img/driver_icon.png" alt="user" width="50px"></button>
                 <div class="dropdown-content">
@@ -16,15 +16,15 @@
 <div class="sidebar">
     <ul>
         <li> <a href="dash.php">DASHBOARD</a></li>
-        <li><a href="">EDIT PROFILE</a></li>
-        <li><a href="">UPDATE LOCATION</a></li>
-        <li> ORDERS
+        <li><a href="dash_include/edit_profile.php">Edit Profile</a></li>
+        <li><a href="dash_include/update_location.php">Update Location</a></li>
+        <li>Orders
             <ul style="display: none;">
-                <li><a href="">DELIVERY STATUS</a></li>
-                <li><a href="">COMPLETED ORDERS</a></li>
-                <li><a href="">EARNINGS</a></li>
+                <li><a href="dash_include/accept_orders.php">Order Inbox</a></li>
+                <li><a href="dash_include/delivery_history.php">Delivery History</a></li>
+                <li><a href="dash_include/earnings.php">Earnings</a></li>
             </ul>
         </li>
-        <li><a href="">CONTACT ADMINISTRATOR</a></li>
+        <li><a href="dash_include/contact_admin.php">Contact Administrator</a></li>
     </ul>
 </div>

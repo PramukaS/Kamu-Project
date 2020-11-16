0d6aa8e4014b1caab05c1b2474363a52095caa99
Pramuka Seneviratne <41991286+PramukaS@users.noreply.github.com>
2020-11-16 18:01:42 +0530

    update blog

--- Pramuka/blog.php
+++ Pramuka/blog.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Kamu.lk | Blog</title>
-        <link rel="stylesheet" href="css/style.css">
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;0,800;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
-    </head>
-    <body>
+<head>
+	<title>Kamu.lk | Blog</title>
+	<link rel="stylesheet" type="text/css" href="css/blogstyle.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,500" rel="stylesheet">
+</head>
+	<body>
         <div class="container"> 
             <div class="navbar">
                 <div class="logo">
-                    <img src="images/logo.png" alt="logo" width="125px">
+                    <img class="logo" src="images/logo.png" alt="logo" width="125px">
                 </div>
                 <nav>
                     <ul id="MenuItems">
@@ -23,29 +22,102 @@
                     </ul>
                 </nav>
                 <img src="images/menu.png" class="menu-icon" alt="menu" onclick="menutoggle()">
-            </div>
-            <div class="row">
-                <div class="col-2">
-                <div class="text"><h3>Chorizo & mozzarella gnocchi bake <span style="font-size: 11px">  &#8287 &#8287 &#8287  &#8287 &#8287 &#8287 &#8287 &#8287 &#8287 Date:- 15th Nov</span></h3>
-                    <p style="font-size: 11px"> Author: Pramuka Senaviratna</p>
-                    <p>Step 1 Heat the oil in a medium pan over a medium heat. Fry the onion
-                    and garlic for 8-10 mins until soft. Add the chorizo and fry for 5
-                    mins more. Tip in the tomatoes and sugar, and season. Bring to a
-                    simmer, then add the gnocchi and cook for 8 mins, stirring often,
-                    until soft. Heat the grill to high.<span id="dots">...</span><span id="more"><br><br>Step 2 Stir ¾ of the mozzarella and most of the basil through the
-                    gnocchi. Divide the mixture between six ovenproof ramekins, or
-                    put in one baking dish. Top with the remaining mozzarella, then
-                    grill for 3 mins, or until the cheese is melted and golden. Season,
-                    scatter over the remaining basil and serve with green salad.</span></p></div>
-                    <a href="#SignUp" class="btn">Get Start &#8594;</a> 
-                </div>
-                <div class="col-2">
-                    <img src="images/blog/Food1.jpg" alt="healthy food">
-                </div>
-            </div>
-        </div>
-        
-        <Script>
+			</div>
+		</div>
+
+	<h1>Kamu.lk Food Library</h1><br>
+		<div class="small-container">
+			<img src="images/blog/Food1.jpg">
+			<div class="text"><h3>Chorizo & mozzarella gnocchi bake <span style="font-size: 16px">  &#8287 &#8287 &#8287  &#8287 &#8287 &#8287 &#8287 &#8287 &#8287 Date:- 15th Nov</span></h3>
+			<p style="font-size: 16px"> Author: Pramuka Senaviratna</p>
+			<p>Step 1 Heat the oil in a medium pan over a medium heat. Fry the onion
+			and garlic for 8-10 mins until soft. Add the chorizo and fry for 5
+			mins more. Tip in the tomatoes and sugar, and season. Bring to a
+			simmer, then add the gnocchi and cook for 8 mins, stirring often,
+			until soft. Heat the grill to high.<span id="dots">...</span><span id="more"><br><br>Step 2 Stir ¾ of the mozzarella and most of the basil through the
+			gnocchi. Divide the mixture between six ovenproof ramekins, or
+			put in one baking dish. Top with the remaining mozzarella, then
+			grill for 3 mins, or until the cheese is melted and golden. Season,
+			scatter over the remaining basil and serve with green salad.</span></p></div>
+
+			<br><button onclick="myFunction()" id="myBtn">More</button>
+
+			<img src="images/blog/Food2.jpg">
+			<div class="text">
+		    <h3>Lasagna <span style="font-size: 16px">  &#8287 &#8287 &#8287  &#8287 &#8287 &#8287 &#8287 &#8287 &#8287 Date:- 12th Nov</span></h3>
+		    <p style="font-size: 16px"> Author: Shifna Shafeek</p>
+			<p>For this recipe, we are essentially making a thick, meaty tomato sauce and layering that with noodles and cheese into a casserole. Here’s the run-down:<br>
+			Start by making the sauce with ground beef, bell peppers, onions, and a combo of tomato sauce, tomato paste, and crushed tomatoes. The three kinds of tomatoes gives the sauce great depth of flavor.
+			<span id="dots">...</span><span id="more">Let this simmer while you boil the noodles and get the cheeses ready. We’re using ricotta, shredded mozzarella, and parmesan — like the mix of tomatoes, this 3-cheese blend gives the lasagna great flavor!<br>
+			From there, it’s just an assembly job. A cup of meat sauce, a layer of noodles, more sauce, followed by a layer of cheese. Repeat until you have three layers and have used up all the ingredients. <br> <br>
+			To freeze an unbaked lasagna: Line the pan with foil before assembling, then assemble the lasagna as directly. Let cool completely, and then freeze until solid. Once frozen, lift the frozen lasagna block from the casserole dish, wrap it in more foil, then freeze for up to a month.<br><br>
+			To reheat an unbaked frozen lasagna: Remove the lasagna from the freezer and unwrap all the layers of foil. Transfer to the original casserole dish, cover, and let it thaw in the fridge overnight. Once thawed, bake as directed.
+			To freeze baked lasagna: Line the pan with foil before assembling, then assemble and bake as directed. Let it cool completely, then freeze until solid. Once frozen, lift the frozen lasagna block from the casserole dish, wrap it in more foil, then freeze for up to a month. <br><br>
+			Bake until bubbly and you’re ready to eat!</span></p>
+			
+			</div>
+			<button onclick="myFunction()" id="myBtn2">More</button>
+			
+
+			<img src="images/blog/Food3.jpg">
+			<div class="text">
+			<h3>Butter garlic crab <span style="font-size: 16px">  &#8287 &#8287 &#8287  &#8287 &#8287 &#8287 &#8287 &#8287 &#8287 Date:- 07th Nov</span></h3>
+			<p style="font-size: 16px"> Author: Pathika Weererathna</p>
+			<p>For this recipe, we are essentially making a thick, meaty tomato sauce and layering that with noodles and cheese into a casserole. Here’s the run-down:<br>
+			Start by making the sauce with ground beef, bell peppers, onions, and a combo of tomato sauce, tomato paste, and crushed tomatoes. The three kinds of tomatoes gives the sauce great depth of flavor.
+			<span id="dots">...</span><span id="more">Let this simmer while you boil the noodles and get the cheeses ready. We’re using ricotta, shredded mozzarella, and parmesan — like the mix of tomatoes, this 3-cheese blend gives the lasagna great flavor!<br>
+			From there, it’s just an assembly job. A cup of meat sauce, a layer of noodles, more sauce, followed by a layer of cheese. Repeat until you have three layers and have used up all the ingredients. <br> <br>
+			To freeze an unbaked lasagna: Line the pan with foil before assembling, then assemble the lasagna as directly. Let cool completely, and then freeze until solid. Once frozen, lift the frozen lasagna block from the casserole dish, wrap it in more foil, then freeze for up to a month.<br><br>
+			To reheat an unbaked frozen lasagna: Remove the lasagna from the freezer and unwrap all the layers of foil. Transfer to the original casserole dish, cover, and let it thaw in the fridge overnight. Once thawed, bake as directed.
+			To freeze baked lasagna: Line the pan with foil before assembling, then assemble and bake as directed. Let it cool completely, then freeze until solid. Once frozen, lift the frozen lasagna block from the casserole dish, wrap it in more foil, then freeze for up to a month. <br><br>
+			Bake until bubbly and you’re ready to eat!</span></p></div>
+			
+			<button onclick="myFunction()" id="myBtn3">More</button>
+
+
+			
+			<img src="images/blog/Food4.jpg">
+			<div class="text"><h3>Laksa<span style="font-size: 16px">  &#8287 &#8287 &#8287  &#8287 &#8287 &#8287 &#8287 &#8287 &#8287 Date:- 04th Nov</span></h3>
+			<p style="font-size: 16px"> Author: Juzly Ahamed</p>
+			<p>Dishes don’t get much more typically French than boeuf bourguignon. The dish hails from the same region as coq au vin – that’s Burgundy in eastern France – and there are similarities between the two dishes. Boeuf bourguignon is essentially a stew made from beef braised in red wine, beef broth, and seasoned vegetables including pearl onions and mushrooms. Originally a peasant dish, this recipe is now a staple in French restaurants around the world. Traditionally, the cheap cuts of meat would be tenderized in wine for two days to intensify the flavors, although some shortcuts can be taken. Every August in Burgundy, the Fête du Charolais celebrates the dish, along with plenty of music and wine
+			<span id="dots">...</span><span id="more">Let this simmer while you boil the noodles and get the cheeses ready. We’re using ricotta, shredded mozzarella, and parmesan — like the mix of tomatoes, this 3-cheese blend gives the lasagna great flavor!<br>
+			From there, it’s just an assembly job. A cup of meat sauce, a layer of noodles, more sauce, followed by a layer of cheese. Repeat until you have three layers and have used up all the ingredients. <br> <br>
+			To freeze an unbaked lasagna: Line the pan with foil before assembling, then assemble the lasagna as directly. Let cool completely, and then freeze until solid. Once frozen, lift the frozen lasagna block from the casserole dish, wrap it in more foil, then freeze for up to a month.<br><br>
+			To reheat an unbaked frozen lasagna: Remove the lasagna from the freezer and unwrap all the layers of foil. Transfer to the original casserole dish, cover, and let it thaw in the fridge overnight. Once thawed, bake as directed.
+			To freeze baked lasagna: Line the pan with foil before assembling, then assemble and bake as directed. Let it cool completely, then freeze until solid. Once frozen, lift the frozen lasagna block from the casserole dish, wrap it in more foil, then freeze for up to a month. <br><br>
+			Bake until bubbly and you’re ready to eat!</span></p></div>
+
+			<button onclick="myFunction()" id="myBtn4">More</button>
+
+			<img src="images/blog/Food5.jpg">
+			<div class="text"><h3>Confit de canard<span style="font-size: 16px">  &#8287 &#8287 &#8287  &#8287 &#8287 &#8287 &#8287 &#8287 &#8287 Date:- 04th Nov</span></h3>
+			<p style="font-size: 16px"> Author: Juzly Ahamed</p>
+			<p>For this recipe, we are essentially making a thick, meaty tomato sauce and layering that with noodles and cheese into a casserole. Here’s the run-down:<br>
+			Start by making the sauce with ground beef, bell peppers, onions, and a combo of tomato sauce, tomato paste, and crushed tomatoes. The three kinds of tomatoes gives the sauce great depth of flavor.
+			<span id="dots">...</span><span id="more">Let this simmer while you boil the noodles and get the cheeses ready. We’re using ricotta, shredded mozzarella, and parmesan — like the mix of tomatoes, this 3-cheese blend gives the lasagna great flavor!<br>
+			From there, it’s just an assembly job. A cup of meat sauce, a layer of noodles, more sauce, followed by a layer of cheese. Repeat until you have three layers and have used up all the ingredients. <br> <br>
+			To freeze an unbaked lasagna: Line the pan with foil before assembling, then assemble the lasagna as directly. Let cool completely, and then freeze until solid. Once frozen, lift the frozen lasagna block from the casserole dish, wrap it in more foil, then freeze for up to a month.<br><br>
+			To reheat an unbaked frozen lasagna: Remove the lasagna from the freezer and unwrap all the layers of foil. Transfer to the original casserole dish, cover, and let it thaw in the fridge overnight. Once thawed, bake as directed.
+			To freeze baked lasagna: Line the pan with foil before assembling, then assemble and bake as directed. Let it cool completely, then freeze until solid. Once frozen, lift the frozen lasagna block from the casserole dish, wrap it in more foil, then freeze for up to a month. <br><br>
+			Bake until bubbly and you’re ready to eat!</span></p></div>
+
+			<button onclick="myFunction()" id="myBtn4">More</button>
+
+			<img src="images/blog/Food6.jpg">
+			<div class="text"><h3>Boeuf bourguignon<span style="font-size: 16px">  &#8287 &#8287 &#8287  &#8287 &#8287 &#8287 &#8287 &#8287 &#8287 Date:- 04th Nov</span></h3>
+			<p style="font-size: 16px"> Author: Juzly Ahamed</p>
+			<p>Dishes don’t get much more typically French than boeuf bourguignon. The dish hails from the same region as coq au vin – that’s Burgundy in eastern France – and there are similarities between the two dishes. Boeuf bourguignon is essentially a stew made from beef braised in red wine, beef broth, and seasoned vegetables including pearl onions and mushrooms. Originally a peasant dish, this recipe is now a staple in French restaurants around the world. Traditionally, the cheap cuts of meat would be tenderized in wine for two days to intensify the flavors, although some shortcuts can be taken. Every August in Burgundy, the Fête du Charolais celebrates the dish, along with plenty of music and wine
+			<span id="dots">...</span><span id="more">Let this simmer while you boil the noodles and get the cheeses ready. We’re using ricotta, shredded mozzarella, and parmesan — like the mix of tomatoes, this 3-cheese blend gives the lasagna great flavor!<br>
+			From there, it’s just an assembly job. A cup of meat sauce, a layer of noodles, more sauce, followed by a layer of cheese. Repeat until you have three layers and have used up all the ingredients. <br> <br>
+			To freeze an unbaked lasagna: Line the pan with foil before assembling, then assemble the lasagna as directly. Let cool completely, and then freeze until solid. Once frozen, lift the frozen lasagna block from the casserole dish, wrap it in more foil, then freeze for up to a month.<br><br>
+			To reheat an unbaked frozen lasagna: Remove the lasagna from the freezer and unwrap all the layers of foil. Transfer to the original casserole dish, cover, and let it thaw in the fridge overnight. Once thawed, bake as directed.
+			To freeze baked lasagna: Line the pan with foil before assembling, then assemble and bake as directed. Let it cool completely, then freeze until solid. Once frozen, lift the frozen lasagna block from the casserole dish, wrap it in more foil, then freeze for up to a month. <br><br>
+			Bake until bubbly and you’re ready to eat!</span></p></div>
+
+			<button onclick="myFunction()" id="myBtn4">More</button>
+		</div>
+		
+		<script>
             var MenuItems = document.getElementById("MenuItems");
             MenuItems.style.maxHeight = "0px";
             
@@ -58,21 +130,21 @@
                 }
             } 
 
-            myFunction() {
-                var dots = document.getElementById("dots");
-                var moreText = document.getElementById("more");
-                var btnText = document.getElementById("myBtn");
+			function myFunction() {
+				var dots = document.getElementById("dots");
+				var moreText = document.getElementById("more");
+				var btnText = document.getElementById("myBtn");
 
-                if (dots.style.display === "none") {
-                    dots.style.display = "inline";
-                    btnText.innerHTML = "More";
-                    moreText.style.display = "none";
-                } else {
-                    dots.style.display = "none";
-                    btnText.innerHTML = "Less";
-                    moreText.style.display = "inline";
-                }
-        </Script>
-
-    </body>
+				if (dots.style.display === "none") {
+					dots.style.display = "inline";
+					btnText.innerHTML = "More";
+					moreText.style.display = "none";
+				} else {
+					dots.style.display = "none";
+					btnText.innerHTML = "Less";
+					moreText.style.display = "inline";
+				}
+			}
+		</script>
+	</body>
 </html>
